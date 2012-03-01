0878ca675b1a972276eb0805538bbaa7cfc49640
Nathan Smith <nwsmith@gmail.com>
2012-02-29 17:27:35 -0700

    dag

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RecruitingOnRails::Application.config.secret_token = 'cec1c0e9042df8b3a6fb8cc9b758002df2abcb58481036c26c1814746e1efb2e98a2af948efdc2ac2aad2f4da4770a7f65d75426b75cc169a1a699c8fcb37a32'
--- lib/auth_config.rb
+++
@@ -1,9 +0,0 @@
-module AuthConfig
-  def config
-    @@config ||= {}
-  end
-
-  def config=(hash)
-    @@config = hash
-  end
-end
