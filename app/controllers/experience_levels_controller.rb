38feeaa060b231d4bf0b31d376620f4300a6cd7b
Nathan Smith <nwsmith@gmail.com>
2012-03-23 12:50:49 -0600

    Adding idea of experience levels

---
+++ app/controllers/experience_levels_controller.rb
@@ -0,0 +1,83 @@
+class ExperienceLevelsController < ApplicationController
+  # GET /experience_levels
+  # GET /experience_levels.json
+  def index
+    @experience_levels = ExperienceLevel.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @experience_levels }
+    end
+  end
+
+  # GET /experience_levels/1
+  # GET /experience_levels/1.json
+  def show
+    @experience_level = ExperienceLevel.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @experience_level }
+    end
+  end
+
+  # GET /experience_levels/new
+  # GET /experience_levels/new.json
+  def new
+    @experience_level = ExperienceLevel.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @experience_level }
+    end
+  end
+
+  # GET /experience_levels/1/edit
+  def edit
+    @experience_level = ExperienceLevel.find(params[:id])
+  end
+
+  # POST /experience_levels
+  # POST /experience_levels.json
+  def create
+    @experience_level = ExperienceLevel.new(params[:experience_level])
+
+    respond_to do |format|
+      if @experience_level.save
+        format.html { redirect_to @experience_level, notice: 'Experience level was successfully created.' }
+        format.json { render json: @experience_level, status: :created, location: @experience_level }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @experience_level.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /experience_levels/1
+  # PUT /experience_levels/1.json
+  def update
+    @experience_level = ExperienceLevel.find(params[:id])
+
+    respond_to do |format|
+      if @experience_level.update_attributes(params[:experience_level])
+        format.html { redirect_to @experience_level, notice: 'Experience level was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @experience_level.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /experience_levels/1
+  # DELETE /experience_levels/1.json
+  def destroy
+    @experience_level = ExperienceLevel.find(params[:id])
+    @experience_level.destroy
+
+    respond_to do |format|
+      format.html { redirect_to experience_levels_url }
+      format.json { head :no_content }
+    end
+  end
+end
---
+++ db/migrate/20120323183747_create_experience_levels.rb
@@ -0,0 +1,34 @@
+class CreateExperienceLevels < ActiveRecord::Migration
+  def up
+    create_table :experience_levels do |t|
+      t.string :code
+      t.string :name
+      t.string :description
+    end
+
+    change_table :candidates do |t|
+      t.references :experience_level
+    end
+
+    execute <<-SQL
+      ALTER TABLE candidates
+        ADD CONSTRAINT fk_candidate_exp_lvls
+        FOREIGN KEY (experience_level_id)
+        REFERENCES experience_levels(id)
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      ALTER TABLE candidates
+        DROP FOREIGN KEY fk_candidate_exp_lvls
+    SQL
+
+    change_table :candidates do |t|
+      remove_column :experience_levels_id
+    end
+
+    drop_table :experience_levels
+  end
+
+end
