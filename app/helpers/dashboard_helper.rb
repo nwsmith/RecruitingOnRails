6ae867c654f6fbd0c9e7d9b55dde3c1e369c5baf
Nathan Smith <nwsmith@gmail.com>
2012-03-28 13:59:10 -0600

    - add submissions and interviews to dashboard

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -3,13 +3,34 @@ module DashboardHelper
     candidates = args.first
     #opts = args.second || {}
 
+    interview_column_count = 0
+    candidates.each {|c| interview_column_count = c.interviews.length if c.interviews.length > interview_column_count}
+    code_submission_column_count = 0
+    candidates.each {|s| code_submission_column_count = s.code_submissions.length if s.code_submissions.length > code_submission_column_count}
+
     out = "<table>"
     out += "<th>Name</th>"
+    1.upto(code_submission_column_count) do |i|
+      out += "<th>Code Submission #{i}</th>"
+    end
+    1.upto(interview_column_count) do |i|
+      out += "<th>Interview #{i}</th>"
+    end
     candidates.each do |candidate|
       out += "<tr>"
       out += "<td>"
       out += link_to candidate.name, candidate_path(candidate)
       out += "</td>"
+      0.upto(code_submission_column_count-1) do |i|
+        out += "<td>"
+        out += candidate.code_submissions[i].nil? ? '&nbsp;' : (link_to candidate.code_submissions[i].name, code_submission_path(candidate.code_submissions[i]))
+        out += "</td>"
+      end
+      0.upto(interview_column_count-1) do |i|
+        out += "<td>"
+        out += candidate.interviews[i].nil? ? '&nbsp;' : (link_to candidate.interviews[i].name, interview_path(candidate.interviews[i]))
+        out += "</td>"
+      end
       out += "</tr>"
     end
     out += "</table>"
