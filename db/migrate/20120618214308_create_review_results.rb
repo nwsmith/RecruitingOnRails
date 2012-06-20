f03a93471a1aaab7ab4713332fd765e5f140739f
Nathan Smith <nwsmith@gmail.com>
2012-06-20 09:32:54 -0600

    - fix review result migration

--- db/migrate/20120618214308_create_review_results.rb
+++ db/migrate/20120618214308_create_review_results.rb
@@ -30,60 +30,9 @@ class CreateReviewResults < ActiveRecord::Migration
         REFERENCES review_results(id)
     SQL
 
-    approved = ReviewResult.create({code: 'APPROVED', name: 'Thumbs Up', is_approval: true, is_disapproval: false})
-    unapproved = ReviewResult.create({code: 'REJECTED', name: 'Thumbs Down', is_approval: false, is_disapproval: true})
-    no_review = ReviewResult.create({code: 'NO_REVIEW', name: 'N/A', is_approval: false, is_disapproval: false})
-
-    InterviewReview.all.each do |interview_review|
-      if interview_review.approved?
-        interview_review.review_result_id = approved.id
-      elsif interview_review.unapproved?
-        interview_review.review_result_id = unapproved.id
-      else
-        interview_review.review_result_id = no_review
-      end
-      interview_review.save!
-    end
-
-    CodeSubmissionReview.all.each do |submission_review|
-      if submission_review.approved?
-        submission_review.review_result_id = approved.id
-      elsif submission_review.unapproved?
-        submission_review.review_result_id = unapproved.id
-      else
-        submission_review.review_result_id = no_review
-      end
-      submission_review.save!
-    end
-
-    remove_column :interview_reviews, :approved
-    remove_column :interview_reviews, :unapproved
-    remove_column :code_submission_reviews, :approved
-    remove_column :code_submission_reviews, :unapproved
   end
 
   def down
-    add_column :interview_reviews, :approved, :boolean
-    add_column :interview_reviews, :unapproved, :boolean
-    add_column :code_submission_reviews, :approved, :boolean
-    add_column :code_submission_reviews, :unapproved, :boolean
-
-    approved = ReviewResult.find_by_code('APPROVED')
-    unapproved = ReviewResult.find_by_code('REJECTED')
-
-    # The "no review case" would be covered by neither column being set to true, which was the old (ugly) way
-    InterviewReview.all.each do |interview_review|
-      interview_review.approved = true if !interview_review.review_result.nil? && interview_review.review_result.id == approved.id
-      interview_review.unapproved = true if !interview_review.review_result.nil? && interview_review.review_result.id == unapproved.id
-      interview_review.save!
-    end
-
-    CodeSubmissionReview.all.each do |submission_review|
-      submission_review.approved = true if !submission_review.review_result.nil? && submission_review.review_result.id == approved.id
-      submission_review.unapproved = true if !submission_review.review_result.nil? && submission_review.review_result.id == unapproved.id
-      submission_review.save!
-    end
-
     execute <<-SQL
       ALTER TABLE code_submission_reviews
         DROP FOREIGN KEY fk_submission_rev_res
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120618214308) do
+ActiveRecord::Schema.define(:version => 20120620152856) do
 
   create_table "candidate_sources", :force => true do |t|
     t.string "code"
