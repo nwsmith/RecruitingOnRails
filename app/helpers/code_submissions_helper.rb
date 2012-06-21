6e841f683ce9e2d0efd145a742a8c021a0486c6c
Nathan Smith <nwsmith@gmail.com>
2012-06-21 10:58:16 -0600

    - add age to code submissions in dashboard - cleanup helper code a bit

--- app/helpers/code_submissions_helper.rb
+++ app/helpers/code_submissions_helper.rb
@@ -1,2 +1,20 @@
 module CodeSubmissionsHelper
+  def format_submission(*args)
+    code_submission = args.first
+    #opts = args.second || {}
+
+    return '' if code_submission.nil?
+
+    out = code_submission.name
+
+    unless code_submission.sent_date.nil?
+      if code_submission.submission_date.nil?
+        out += "(#{distance_of_time_in_words_to_now(code_submission.sent_date)}+)"
+      else
+        out += "(#{distance_of_time_in_words(code_submission.sent_date, code_submission.submission_date)})"
+      end
+    end
+
+    link_to approved_span(code_submission, :text => out), code_submission_path(code_submission)
+  end
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,47 +1,4 @@
 module DashboardHelper
-  def candidates_table(*args)
-    candidates = args.first
-    #opts = args.second || {}
-
-    interview_column_count = 0
-    candidates.each {|c| interview_column_count = c.interviews.length if c.interviews.length > interview_column_count}
-    code_submission_column_count = 0
-    candidates.each {|s| code_submission_column_count = s.code_submissions.length if s.code_submissions.length > code_submission_column_count}
-
-    out = "<table>"
-    out += "<tr>"
-    out += "<th>Name</th>"
-    1.upto(code_submission_column_count) do |i|
-      out += "<th>Code Submission #{i}</th>"
-    end
-    1.upto(interview_column_count) do |i|
-      out += "<th>Interview #{i}</th>"
-    end
-    out += "<th>References?</th>"
-    out += "</tr>"
-    candidates.each do |candidate|
-      out += "<tr>"
-      out += "<td>"
-      out += link_to(color_span(candidate.experience_level, candidate.name), candidate_path(candidate))
-      out += "</td>"
-      0.upto(code_submission_column_count-1) do |i|
-        out += "<td>"
-        out += candidate.code_submissions[i].nil? ? '&nbsp;' : (link_to approved_span(candidate.code_submissions[i]), code_submission_path(candidate.code_submissions[i]))
-        out += "</td>"
-      end
-      0.upto(interview_column_count-1) do |i|
-        out += "<td>"
-        out += candidate.interviews[i].nil? ? '&nbsp;' : (link_to approved_span(candidate.interviews[i]), interview_path(candidate.interviews[i]))
-        out += "</td>"
-      end
-      out += "<td>"
-      out += approved_span(candidate.reference_checks, :text => candidate.reference_checks.length)
-      out += "</td>"
-      out += "</tr>"
-    end
-    out += "</table>"
-    out.html_safe
-  end
 
   def counts_by_status
     candidates_by_status = Hash.new
