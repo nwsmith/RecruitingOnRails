a48e92081d4229edfa01b3f1c34039d146cc82dd
Nathan Smith <nwsmith@gmail.com>
2013-02-08 13:32:09 -0700

    - fix status bug that was crashing dashboard.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -3,7 +3,7 @@ module DashboardHelper
   def counts_by_status
     candidates_by_status = Hash.new
     CandidateStatus.all.each {|status| candidates_by_status[status] = 0}
-    Candidate.all.each {|candidate| candidates_by_status[candidate.candidate_status] += 1}
+    Candidate.all.each {|candidate| candidates_by_status[candidate.candidate_status] = candidates_by_status[candidate.candidate_status].nil? ? 0 : candidates_by_status[candidate.candidate_status] + 1}
 
     out = "<table>"
     out += "<th>Status</th><th>Count</th>"
