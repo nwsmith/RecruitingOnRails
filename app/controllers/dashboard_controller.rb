c4a11250c82a9063a816ec4c0577ecf1fc092526
Nathan Smith <nathan.smith@yyc-nsmith-mp.sumyyc01.local>
2015-10-08 09:10:28 -0600

    - Add headcount target and messaging for what we are looking for to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,15 @@
 class DashboardController < ApplicationController
   def index
+    headcount_target_registry = Registry.find_by_key('team.headcount_target')
+
+    if headcount_target_registry.nil?
+      @headcount_target = nil
+    else
+      @headcount_target = headcount_target_registry.value.to_i
+      @hired_count = Candidate.by_status_code('HIRED').length
+      @headcount_left = @headcount_target - @hired_count
+    end
+
     default_status = Registry.find_by_key('dashboard.default_status')
     @candidates_by_status = Candidate.by_status_code(default_status.value)
   end
