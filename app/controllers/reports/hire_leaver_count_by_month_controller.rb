2d966e2914908ab3e5d6252b6d77f7679172c8fe
Nathan Smith <nathan.smith@yyc-nsmith-mp.local>
2016-06-18 20:50:42 -0600

    - Hire/Leaver count by month report

--- app/controllers/reports/hire_leaver_count_by_month_controller.rb
+++ app/controllers/reports/hire_leaver_count_by_month_controller.rb
@@ -4,20 +4,21 @@ class Reports::HireLeaverCountByMonthController < ApplicationController
 
   def run
     @table = Reports::ReportTable.new('Hire Count By Month')
-    @table.header = ['Month', 'Count', 'Lame Graphic', 'Data']
+    @table.header = ['Month', 'Count', 'Lame Graphic']
 
     @table1 = Reports::ReportTable.new('Leaver Count By Month')
-    @table1.header = ['Month', 'Count', 'Lame Graphic', 'Data']
+    @table1.header = ['Month', 'Count', 'Lame Graphic']
 
     period_info = Reports::PeriodInfo.new
     period_info.add_candidates(Candidate.all)
 
-    hires_by_month = leavers_by_month = {}
+    hires_by_month = {}
+    leavers_by_month = {}
 
-    1.upto(12) { |m| hires_by_month[m] = leavers_by_month[m] = 0}
+    1.upto(12) { |m| hires_by_month[m] = leavers_by_month[m] = 0 }
 
-    period_info.hired_list_ignore_left.each { |c| hires_by_month[c.start_date.month] += 1}
-    period_info.left_list.each { |c| leavers_by_month[c.end_date.month] += 1}
+    period_info.hired_list_ignore_left.each { |c| hires_by_month[c.start_date.month] += 1 }
+    period_info.left_list.each { |c| leavers_by_month[c.end_date.month] += 1 }
 
     hires_by_month.keys.sort.each do |m|
       @table.rows << [Date.new(2016, m).strftime('%b'), hires_by_month[m], "#{'*' * hires_by_month[m]}"]
