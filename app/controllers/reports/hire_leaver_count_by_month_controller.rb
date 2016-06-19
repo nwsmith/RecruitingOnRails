a437f97128951619822c333f94683a3dfcdb76c3
Nathan Smith <nathan.smith@yyc-nsmith-mp.local>
2016-06-18 20:11:42 -0600

    - Hire/Leaver count by month report

--- app/controllers/reports/hire_leaver_count_by_month_controller.rb
+++ app/controllers/reports/hire_leaver_count_by_month_controller.rb
@@ -12,8 +12,6 @@ class Reports::HireLeaverCountByMonthController < ApplicationController
     period_info = Reports::PeriodInfo.new
     Candidate.all.each { |c| period_info.add_candidate(c) unless c.start_date.nil? }
 
-    @table.title = period_info.inspect
-
     hires_by_month = {}
     leavers_by_month = {}
 
@@ -22,11 +20,12 @@ class Reports::HireLeaverCountByMonthController < ApplicationController
     period_info.hired_list_ignore_left.each { |c| hires_by_month[c.start_date.month] += 1 }
     period_info.left_list.each { |c| leavers_by_month[c.start_date.month] += 1 }
 
-    1.upto(12) do |m|
-      d = Date.new(2016, m).strftime('%b')
+    hires_by_month.keys.sort.each do |m|
+      @table.rows << [Date.new(2016, m).strftime('%b'), hires_by_month[m], "#{'*' * hires_by_month[m]}"]
+    end
 
-      @table.rows << [d, hires_by_month[m], "#{'*' * hires_by_month[m]}"]
-      @table1.rows << [d, leavers_by_month[m], "#{'*' * leavers_by_month[m]}"]
+    leavers_by_month.keys.sort.each do |m|
+      @table1.rows << [Date.new(2016, m).strftime('%b'), leavers_by_month[m], "#{'*' * hires_by_month[m]}"]
     end
   end
 end
