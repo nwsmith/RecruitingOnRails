71b2085338385066f12b2847d8d618256398b2a4
Nathan Smith <nathan.smith@yyc-nsmith-mp.local>
2016-06-18 20:40:51 -0600

    - Hire/Leaver count by month report

--- app/controllers/reports/hire_leaver_count_by_month_controller.rb
+++ app/controllers/reports/hire_leaver_count_by_month_controller.rb
@@ -23,7 +23,7 @@ class Reports::HireLeaverCountByMonthController < ApplicationController
     1.upto(12) { |m| hires_by_month[m] = 0; hn_by_month[m] = []; leavers_by_month[m] = 0; lv_by_month[m] = [] }
 
     period_info.hired_list_ignore_left.each { |c| hires_by_month[c.start_date.month] += 1; hn_by_month[c.start_date.month] << c.first_name }
-    period_info.left_list.each { |c| leavers_by_month[c.start_date.month] += 1; lv_by_month[c.start_date.month] << c.first_name}
+    period_info.left_list.each { |c| leavers_by_month[c.end_date.month] += 1; lv_by_month[c.end_date.month] << c.first_name}
 
     hires_by_month.keys.sort.each do |m|
       @table.rows << [Date.new(2016, m).strftime('%b'), hires_by_month[m], "#{'*' * hires_by_month[m]}", hn_by_month[m].join(',')]
