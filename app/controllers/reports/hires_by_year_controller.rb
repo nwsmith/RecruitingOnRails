08e3d19fadc5c78d2013d24393e5480766627b62
Nathan Smith <nathan.smith@yyc-nsmith-mp.local>
2016-06-18 19:32:22 -0600

    - Add Leavers by Year to the Hires By Year report

--- app/controllers/reports/hires_by_year_controller.rb
+++ app/controllers/reports/hires_by_year_controller.rb
@@ -6,12 +6,19 @@ class Reports::HiresByYearController < ApplicationController
     @table = Reports::ReportTable.new('Hires By Year')
     @table.header = %w(Year Hires)
 
+    @table1 = Reports::ReportTable.new('Leavers By Year')
+    @table1.header = %w(Year Hires)
+
     period_info = Reports::PeriodInfo.new
     Candidate.all.each {|c| period_info.add_candidate(c) unless c.start_date.nil?}
     period_info.year_info_map.values.sort {|a,b| a.year <=> b.year}.each do |year_info|
       dev_list = []
       year_info.still_here.sort_by{|d| d.start_date}.each {|d| dev_list << "#{d.first_name} #{d.last_name} (#{d.start_date.strftime('%b')}, #{d.tenure_in_years})"}
       @table.rows << [year_info.year, dev_list.join(',')]
+
+      dev_list1 = []
+      year_info.left_in_year.sort_by{|d| d.end_date}.each {|d| dev_list1 << "#{d.first_name} #{d.last_name} (#{d.end_date.strftime('%b')}, #{d.tenure_in_years})"}
+      @table1.rows << [year_info.year, dev_list1.join(',')]
     end
   end
 end
