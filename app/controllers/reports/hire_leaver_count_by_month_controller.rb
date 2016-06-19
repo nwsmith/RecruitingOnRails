c53eba64d21148b4659dd3898a8510d7ea49c782
Nathan Smith <nathan.smith@yyc-nsmith-mp.local>
2016-06-18 20:34:24 -0600

    - Hire/Leaver count by month report

--- app/controllers/reports/hire_leaver_count_by_month_controller.rb
+++ app/controllers/reports/hire_leaver_count_by_month_controller.rb
@@ -4,10 +4,10 @@ class Reports::HireLeaverCountByMonthController < ApplicationController
 
   def run
     @table = Reports::ReportTable.new('Hire Count By Month')
-    @table.header = ['Month', 'Count', 'Lame Graphic']
+    @table.header = ['Month', 'Count', 'Lame Graphic', 'Data']
 
     @table1 = Reports::ReportTable.new('Leaver Count By Month')
-    @table1.header = ['Month', 'Count', 'Lame Graphic']
+    @table1.header = ['Month', 'Count', 'Lame Graphic', 'Data']
 
     period_info = Reports::PeriodInfo.new
 
@@ -17,18 +17,20 @@ class Reports::HireLeaverCountByMonthController < ApplicationController
 
     hires_by_month = {}
     leavers_by_month = {}
+    hn_by_month = {}
+    lv_by_month = {}
 
-    1.upto(12) { |m| hires_by_month[m] = 0; leavers_by_month[m] = 0 }
+    1.upto(12) { |m| hires_by_month[m] = 0; hn_by_month[m] = []; leavers_by_month[m] = 0; lv_by_month[m] }
 
-    period_info.hired_list_ignore_left.each { |c| hires_by_month[c.start_date.month] += 1 }
-    period_info.left_list.each { |c| leavers_by_month[c.start_date.month] += 1 }
+    period_info.hired_list_ignore_left.each { |c| hires_by_month[c.start_date.month] += 1; hn_by_month[m] << c.first_name }
+    period_info.left_list.each { |c| leavers_by_month[c.start_date.month] += 1; lv_by_month[m] << c.first_name}
 
     hires_by_month.keys.sort.each do |m|
-      @table.rows << [Date.new(2016, m).strftime('%b'), hires_by_month[m], "#{'*' * hires_by_month[m]}"]
+      @table.rows << [Date.new(2016, m).strftime('%b'), hires_by_month[m], "#{'*' * hires_by_month[m]}", hn_by_month[m].join(",")]
     end
 
     leavers_by_month.keys.sort.each do |m|
-      @table1.rows << [Date.new(2016, m).strftime('%b'), leavers_by_month[m], "#{'*' * leavers_by_month[m]}"]
+      @table1.rows << [Date.new(2016, m).strftime('%b'), leavers_by_month[m], "#{'*' * leavers_by_month[m]}", lv_by_month[m].join(',')]
     end
   end
 end
