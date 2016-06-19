9412927e82b3d985c876f6890390eb242d455d73
Nathan Smith <nathan.smith@yyc-nsmith-mp.local>
2016-06-18 19:59:11 -0600

    - Hire/Leaver count by month report

--- app/controllers/reports/hire_leaver_count_by_month_controller.rb
+++ app/controllers/reports/hire_leaver_count_by_month_controller.rb
@@ -11,10 +11,14 @@ class Reports::HireLeaverCountByMonthController < ApplicationController
 
     period_info = Reports::PeriodInfo.new
     Candidate.all.each { |c| period_info.add_candidate(c) unless c.start_date.nil? }
-    hires_by_month = leavers_by_month = {}
-    1.upto(12) { |m| hires_by_month[m] = leavers_by_month[m] = 0 }
-    period_info.hired_list_ignore_left.each { |d| hires_by_month[d.start_date.month] += 1 }
-    period_info.left_list.each { |d| leavers_by_month[d.start_date.month] += 1 }
+
+    hires_by_month = {}
+    leavers_by_month = {}
+
+    1.upto(12) { |m| hires_by_month[m] = 0; leavers_by_month[m] = 0 }
+
+    period_info.hired_list_ignore_left.each { |c| hires_by_month[c.start_date.month] += 1 }
+    period_info.left_list.each { |c| leavers_by_month[c.start_date.month] += 1 }
 
     1.upto(12) do |m|
       d = Date.new(2016, m).strftime('%b')
