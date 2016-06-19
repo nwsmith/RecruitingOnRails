de2e2a5ccd5fd51825061b32c9ceaf557f88f5e2
Nathan Smith <nathan.smith@yyc-nsmith-mp.local>
2016-06-18 20:26:38 -0600

    - Hire/Leaver count by month report

--- lib/reports.rb
+++ lib/reports.rb
@@ -36,13 +36,13 @@ module Reports
       return if start_year.nil?
 
       start_year_info = get(start_year)
-      start_year_info.started_in_year << candidate
+      start_year_info.started_in_year << candidate unless start_year_info.started_in_year.contains? candidate
 
       if end_year.nil?
-        start_year_info.still_here << candidate
+        start_year_info.still_here << candidate unless start_year_info.still_here.contains? candidate
       else
         end_year_info = get(end_year)
-        end_year_info.left_in_year << candidate
+        end_year_info.left_in_year << candidate unless end_year_info.still_here.contains? candidate
       end
 
     end
