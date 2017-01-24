91d4b80d66eccf31fc9418c1cb8ce36948b42805
Nathan Smith <nathan.smith@yyc-nsmith-mp.sumyyc01.local>
2017-01-24 10:31:25 -0700

    - Modify headcount to track by budget

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,14 +2,32 @@ class DashboardController < ApplicationController
   def index
     @dashboard_data = Hash.new
 
-    headcount_target_registry = Registry.find_by_key('team.headcount_target')
+    associated_budgets = AssociatedBudget.all
+    headcount_targets = Hash.new
 
-    if headcount_target_registry.nil?
-      @dashboard_data[:headcount_target] = nil
+
+
+    associated_budgets.each do |associated_budget|
+      budget_headcount_target_key = 'team.headcount_target.' + associated_budget.code
+      budget_headcount_target_reg = Registry.find_by_key(budget_headcount_target_key)
+
+      budget_candidate_count = Candidate.by_status_code('HIRED').select{|c| c.associated_budget_id.equal?(associated_budget.id)}.size
+
+      unless budget_headcount_target_reg.nil?
+        budget_props = Hash.new
+        budget_props[:name] = associated_budget.name
+        budget_props[:target] = budget_headcount_target_reg.value.to_i
+        budget_props[:current] = budget_candidate_count
+        budget_props[:needed] = budget_props[:target] - budget_props[:current]
+
+        headcount_targets[associated_budget.code] = budget_props
+      end
+    end
+
+    if headcount_targets.empty?
+      @dashboard_data[:headcount_targets] = nil
     else
-      @dashboard_data[:headcount_target] = headcount_target_registry.value.to_i
-      @dashboard_data[:hired_count] = Candidate.by_status_code('HIRED').length
-      @dashboard_data[:headcount_left] = @dashboard_data[:headcount_target] - @dashboard_data[:hired_count]
+      @dashboard_data[:headcount_targets] = headcount_targets
     end
 
     default_status = Registry.find_by_key('dashboard.default_status')
