5bf8bc57554104e12c44255e628f7bec5d5e6f52
Nathan Smith <nathan.smith@yyc-nsmith-mp.local>
2017-02-16 09:26:43 -0700

    - use extension to render markdown tables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -104,7 +104,8 @@ module ApplicationHelper
     extensions = {
         autolink: true,
         superscript: true,
-        disable_indented_code_blocks: true
+        disable_indented_code_blocks: true,
+        tables: true
     }
 
     renderer = Redcarpet::Render::HTML.new(options)
