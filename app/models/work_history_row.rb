075242842a89379056ebce8af204eca8e769beb2
Nathan Smith <nathan.smith@yyc-nsmith-mp.sumyyc01.local>
2017-07-26 18:25:03 -0600

    - use date picker for work history rows - allow no date for end date on work history rows - show "present" when there is no end date

--- app/models/work_history_row.rb
+++ app/models/work_history_row.rb
@@ -3,6 +3,9 @@ class WorkHistoryRow < ApplicationRecord
   belongs_to :previous_employer
 
   def summary
-    "#{start_date.to_s} - #{end_date.to_s} (#{previous_employer.name})"
+    end_date_string = end_date.nil? ? 'Present' : end_date.to_s
+
+
+    "#{start_date.to_s} - #{end_date_string} (#{previous_employer.name})"
   end
 end
